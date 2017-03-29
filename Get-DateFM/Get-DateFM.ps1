5d7cbe41900dae80ab6abb46602f287dad3d13e1
pklingaa <pklingaa@users.noreply.github.com>
2017-03-30 00:43:17 +0200

    Add files via upload

---
+++ Get-DateFM/Get-DateFM.ps1
@@ -0,0 +1,41 @@
+#################################
+##                             ##
+## Function Get-DateFM         ##
+## Created by Panupong Klingaa ##
+## https://github.com/pklingaa ##
+## Date: 2017-03-30            ##
+##                             ##
+#################################
+
+function Get-DateFM {
+	<#
+	.SYNOPSIS
+		Datetime format in 'yyyyMMdd-HHmmss'.
+	.DESCRIPTION
+		Usefull for quick logging current datetime.
+		Save this function in memory and execute function when needed.
+	.EXAMPLE
+		Write-Output "$(Get-DateFM) is the current datetime in yyyyMMdd-HHmmss."
+		20170329-234121 is the current datetime in yyyyMMdd-HHmmss.
+	.EXAMPLE
+		Get-DateFM -Format yyMMdd
+		170329
+
+	.LINK
+		https://github.com/pklingaa/powershell/Get-DateFM
+    #>
+	[cmdletbinding()]
+	param (
+		 $Format=[string]$Format
+	)
+	if ($Format -eq "")
+	{
+		Write-Verbose "Format not set. Using default format"
+		$Format = "yyyyMMdd-HHmmss"
+	}
+	else
+	{
+		Write-Verbose "Format set to [$Format]."
+	}
+	(Get-Date).ToString($Format)
+}
